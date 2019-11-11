544cb3c7c78806c5ddeaee9b9b56c113ff3937aa
Oscar Johanneson <oscar.johanneson@gmail.com>
2019-11-11 17:01:45 +0100

    added and tested functions

--- u1_js_osc_joh.js
+++ u1_js_osc_joh.js
@@ -1,9 +1,13 @@
 window.addEventListener("keydown", function(event){
   if(event.code === "KeyD"){
-  
+    print(randomNumber());
   }else if(event.code === "KeyS"){
-  
+    print(randomNumber());
   }
 });
-
-
+function print(word){
+  return console.log(word);
+}
+function randomNumber(){
+  return Math.floor((Math.random() * 11) + 1);
+}
