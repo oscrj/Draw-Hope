299cff38cce47a2e8ec094b00a9f763f4c9c5ddd
Oscar Johanneson <oscar.johanneson@gmail.com>
2019-11-15 15:04:47 +0100

    update 1.0.0.1

--- u1_js_osc_joh.js
+++ u1_js_osc_joh.js
@@ -1,17 +1,18 @@
 "use strict";   //  defines that JavaScript code should be executed in "strict mode".
-var user = randomNumber();  //  declare variable user and store a random number recived from function RandomNumber.
-var cpu = randomNumber();  //  declare variable cpu and store a random number recived from function RandomNumber.
-var userScore = user;   //  variable wich will store the total user score. 
-var cpuScore = cpu;  //  variable wich will store the total cpu score.
-// call function print and give it the argument inside the parenthesis.
-print("Player drew " + user + "\nPlayer score = " + userScore + "\nComputer drew " + cpu + "\nComputer score = " + cpuScore);
-cpu = randomNumber(); //  store a new random number to cpu.
-cpuScore += cpu;  //  add cpuScore togheter with cpu to get the new score.
-print("Computer drew " + cpu + "\nComputer score = " + cpuScore); //  print to console.
+// Declare vaiables and give them value 0 at the beginning of the game, they will store player and computer total scores.
+var userScore = 0, cpuScore = 0;
+userDraw();  //  call function userDraw to recive a new card which will be added and returnd in users total score.
+cpuDraw();  //  call function cpuDraw to recive a new card which will be added and returnd in cpus total score.
+cpuDraw();
 //  If cpuScore is equal to 21, then the Computer has won!
 if(cpuScore === 21){
+  //  Use window method alert() to give player information regading the outcome of the game.
   alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
 }
+else if(cpuScore > 21){  //  If cpuScore is greater than 21 player win.
+  alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+}
+// call function print and give it the argument inside the parenthesis.
 print("Scores: Player " + userScore + " Computer: " + cpuScore + "\nDraw a new card(D) or stay(S)");
 /** 
  * @desc - makes it possible to lisen to event happening on html page. In this case we are lisening to keybord inputs.
@@ -20,9 +21,7 @@ print("Scores: Player " + userScore + " Computer: " + cpuScore + "\nDraw a new c
 window.addEventListener("keydown", function(event){
   //  if key D is pressed down, this will happen. 
   if(event.code === "KeyD"){
-    user = randomNumber();  //  store a new random number to user.
-    userScore += user;  //  add the new random number stored in user togheter with userScore to get the total user scores.
-    print("Player drew " + user + "\nPlayer score = " + userScore);  //  print to console.
+    userDraw();
     //  if uscerScore is equal to 21 player win.
     if(userScore === 21){
       alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
@@ -33,28 +32,47 @@ window.addEventListener("keydown", function(event){
   }
   //  if key S is pressed down, this will happen.
   else if(event.code === "KeyS"){
-    print("Player Stopped!")  //  print to console. 
+    print("Player Stopped!") 
     //  if cpuScore is less than 17. Computer will draw a new card.
     if(cpuScore < 17){
-      cpu = randomNumber();  //  store a new random number to cpu.
-      cpuScore += cpu;  //  add the new random number stored in cpu togheter with cpuScore to get the total cpu scores.
-      print("Computer drew " + cpu + "\nComputer score " + cpuScore);
+      cpuDraw();
     }
-    //  if (21-userScore) is less than (21-cpuScore) player win.
-    if((21 - userScore) < (21 - cpuScore)){
+    //  if cpuScore is greater than 21 player win.
+    if(cpuScore > 21){  //  if cpuScore is greater than 21 player win.
+      alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    }
+    else if(userScore === cpuScore){  //  if userScore is equal to cpuScore its a draw.
+      alert("It's a draw! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    }
+    else if((21 - userScore) < (21 - cpuScore)){  //  if (21-userScore) is less than (21-cpuScore) player win.
       alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
     }
     else if((21 - userScore) > (21 - cpuScore)){  //  if (21-userScore) is greater than (21-cpuScore) computer win.
       alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
     }
-    else if(userScore === cpuScore){  //  if userScore is equal to cpuScore its a draw.
-      alert("It's a draw! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
-    else if(cpuScore > 21){  //  if cpuScore is greater than 21 player win.
-      alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
   }
 });
+/**
+ * @desc - a function that will draw a new card using randomNumber function and will add this new card to player score.
+ * @return integer - return the new score.
+ */
+function userDraw(){
+  let user = randomNumber();  //  store a random number to variable user.
+  userScore += user;  //  add the new random number stored in user togheter with userScore to get th new total user scores.
+  // call function print and give it the argument inside the parenthesis.
+  print("Player drew " + user + "\nPlayer score = " + userScore);
+  return userScore;  // return the new score.
+}
+/**
+ * @desc - a function that will draw a new card using randomNumber function and will add this new card to computer score.
+ * @return integer - return the new score.
+ */
+function cpuDraw(){
+  let cpu = randomNumber();  //  store a random number to cpu.
+  cpuScore += cpu;  //  add the new random number stored in cpu togheter with cpuScore to get the new total cpu scores.
+  print("Computer drew " + cpu + "\nComputer score " + cpuScore);
+  return cpuScore; // return the new score.
+}
 /**
  * @desc - a function that handels log to console.
  * @param string word - takes a string as a parameter to print in console.
