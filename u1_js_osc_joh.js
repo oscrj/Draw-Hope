f735343194a199521c5e5d792cb3fe80bc921d60
Oscar Johanneson <oscar.johanneson@gmail.com>
2019-11-25 15:00:23 +0100

    update 1.0.1

--- u1_js_osc_joh.js
+++ u1_js_osc_joh.js
@@ -1,19 +1,16 @@
 "use strict";   //  defines that JavaScript code should be executed in "strict mode".
 // Declare vaiables and give them value 0 at the beginning of the game, they will store player and computer total scores.
 var userScore = 0, cpuScore = 0;
-userDraw();  //  call function userDraw to recive a new card which will be added and returnd in users total score.
-cpuDraw();  //  call function cpuDraw to recive a new card which will be added and returnd in cpus total score.
-cpuDraw();
-//  If cpuScore is equal to 21, then the Computer has won!
-if(cpuScore === 21){
-  //  Use window method alert() to give player information regading the outcome of the game.
-  alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+
+function init(){
+  userDraw();  //  call function userDraw to recive a new card which will be added and returnd in users total score.
+  cpuDraw();  //  call function cpuDraw to recive a new card which will be added and returnd in cpus total score.
+  cpuDraw();
+  checkResults();
+  // call function print and give it the argument inside the parenthesis.
+  print("Scores: Player " + userScore + " Computer: " + cpuScore + "\nDraw a new card(D) or stay(S)");
 }
-else if(cpuScore > 21){  //  If cpuScore is greater than 21 player win.
-  alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-}
-// call function print and give it the argument inside the parenthesis.
-print("Scores: Player " + userScore + " Computer: " + cpuScore + "\nDraw a new card(D) or stay(S)");
+
 /** 
  * @desc - makes it possible to lisen to event happening on html page. In this case we are lisening to keybord inputs.
  * @param - takes a keyboard inputs as an argument.
@@ -22,36 +19,19 @@ window.addEventListener("keydown", function(event){
   //  if key D is pressed down, this will happen. 
   if(event.code === "KeyD"){
     userDraw();
-    //  if uscerScore is equal to 21 player win.
-    if(userScore === 21){
-      alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
-    else if(userScore > 21){  //  if userScore is greater than 21 player lose.
-      alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
+    checkResults();
   }
   //  if key S is pressed down, this will happen.
   else if(event.code === "KeyS"){
-    print("Player Stopped!") 
+    print("Player Stopped!")
     //  if cpuScore is less than 17. Computer will draw a new card.
     if(cpuScore < 17){
       cpuDraw();
     }
-    //  if cpuScore is greater than 21 player win.
-    if(cpuScore > 21){  //  if cpuScore is greater than 21 player win.
-      alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
-    else if(userScore === cpuScore){  //  if userScore is equal to cpuScore its a draw.
-      alert("It's a draw! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
-    else if((21 - userScore) < (21 - cpuScore)){  //  if (21-userScore) is less than (21-cpuScore) player win.
-      alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
-    else if((21 - userScore) > (21 - cpuScore)){  //  if (21-userScore) is greater than (21-cpuScore) computer win.
-      alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
-    }
+    finalScore();
   }
 });
+
 /**
  * @desc - a function that will draw a new card using randomNumber function and will add this new card to player score.
  * @return integer - return the new score.
@@ -63,6 +43,7 @@ function userDraw(){
   print("Player drew " + user + "\nPlayer score = " + userScore);
   return userScore;  // return the new score.
 }
+
 /**
  * @desc - a function that will draw a new card using randomNumber function and will add this new card to computer score.
  * @return integer - return the new score.
@@ -73,13 +54,57 @@ function cpuDraw(){
   print("Computer drew " + cpu + "\nComputer score " + cpuScore);
   return cpuScore; // return the new score.
 }
+
+function checkResults(){
+  //  If cpuScore is equal to 21, then the Computer has won!
+  if(cpuScore === 21){
+    //  Use window method alert() to give player information regading the outcome of the game.
+    alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }
+  else if(cpuScore > 21){  //  If cpuScore is greater than 21 player win.
+    alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }
+  //  if uscerScore is equal to 21 player win.
+  if(userScore === 21){
+    alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }
+  else if(userScore > 21){  //  if userScore is greater than 21 player lose.
+    alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }  
+}
+
+function finalScore(){
+  //  if cpuScore is greater than 21 player win.
+  if(cpuScore > 21){
+    alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }
+  else if(userScore === cpuScore){  //  if userScore is equal to cpuScore its a draw.
+    alert("It's a draw! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }
+  else if((21 - userScore) < (21 - cpuScore)){  //  if (21-userScore) is less than (21-cpuScore) player win.
+    alert("You won! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }
+  else if((21 - userScore) > (21 - cpuScore)){  //  if (21-userScore) is greater than (21-cpuScore) computer win.
+    alert("Too Bad! You Lose! \n\nComputer Score: " + cpuScore  + "\n\nPlayer Score: " + userScore);
+    resetGame();
+  }
+}
+
 /**
  * @desc - a function that handels log to console.
- * @param string word - takes a string as a parameter to print in console.
+ * @param string msg - takes a string as a parameter to print in console.
  */
-function print(word){
-  console.log(word);
+function print(msg){
+  console.log(msg);
 }
+
 /**
  * @desc - a function thats random pick a number between 1 and 11.
  * @return integer - return a random number between 1-11
@@ -87,3 +112,11 @@ function print(word){
 function randomNumber(){
   return Math.floor((Math.random() * 11) + 1);
 }
+
+function resetGame(){
+  userScore = 0;
+  cpuScore = 0;
+  init();
+}
+
+document.addEventListener("DOMContentLoaded", init);
